5948f2fa6dc82acd5dbdbd6f4d78eb019a309095
Mohit Rathore <mrmohitrathoremr@gmail.com>
2019-01-20 18:26:37 +0530

    supervisorctl working

--- ngrok_server.sh
+++ ngrok_server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 killall ngrok
-~/ngrok  tcp  22 > /dev/null &
-sleep 2
-ip="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")"
+/home/pi/ngrok start --config=/home/pi/.ngrok2/ngrok.yml ssh &
+
+sleep 10
+ip="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")" && echo $ip
+
+WEBHOOK=$(cat /home/pi/slack_webhook.pvt)
 curl -X POST -H 'Content-type: application/json' --data "{\"text\":"$ip"}" $WEBHOOK
